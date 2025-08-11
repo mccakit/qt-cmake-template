a559bb2a70c0bfe2939bc9a5e6801fa86a6d8eaf
mccakit <mccakit09@gmail.com>
2025-08-11 19:00:24 +0300

    l

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
-
+import std;
 int main(int argc, char *argv[])
 {
     QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
+    double d = std::numbers::pi + 1;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     return app.exec();
 }
